f5c13156df45981a43135bb74a00f1838d8d57b0
Solomon Latham <179134799+CosmoCreeper@users.noreply.github.com>
2025-07-30 20:53:06 -0400

    Add debugging for query functionality, change textContent modification to innerHTML

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const themes = await themeData.json();
 
 const displayTheme = (theme) => {
     document.querySelector(".warning").style.color = "white";
-    document.querySelector(".warning").textContent = `
+    document.querySelector(".warning").innerHTML = `
         You have attempted to query the theme: ${theme.name}.
         Due to this site's construction, themes cannot be displayed and
         this query is only for demonstration of query functionality.
@@ -19,6 +19,7 @@ const displayAll = () => {
 const params = new URLSearchParams(window.location.search);
 const query = params.get("theme");
 if (query) {
+    console.log(themes, Object.keys(themes), Object.keys(themes).find(themeId => themeId === query), themes[Object.keys(themes).find(themeId => themeId === query)]);
     const theme = themes[Object.keys(themes).find(themeId => themeId === query)];
     if (theme) {
         displayTheme(theme);
