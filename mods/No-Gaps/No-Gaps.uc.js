3affb646d93909bead126ebdb35e1d2657cf8eab
CompTechGuy <155201407+Comp-Tech-Guy@users.noreply.github.com>
2025-10-13 07:35:20 -0700

    Create No-Gaps.uc.js

---
+++ mods/No-Gaps/No-Gaps.uc.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const containerDiv = document.querySelector('zen-appcontent-navbar-wrapper');
+    const targetDiv = document.querySelector('browser');
+
+    if (containerDiv && targetDiv) {
+        // Get the width of the container div
+        const containerWidth = containerDiv.offsetWidth + 'px';
+
+        // Set the CSS custom property on the target div
+        targetDiv.style.setProperty('--container-width', containerWidth);
+    }
+});
